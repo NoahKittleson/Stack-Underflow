36b46462f91bf102efef00f625b23ae20d388a54
Noah Kittleson <noahkittleson@gmail.com>
2016-06-10 12:04:19 -0700

    added some update functionality to question page

--- app/routes/full-question.js
+++ app/routes/full-question.js
@@ -1,7 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  //showUpdateForm: false,  //fuck, I have put this into a component...
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    update(question) {
+      var params = {
+        author: this.get("author") ? this.get("author") : "",
+        text: this.get("text") ? this.get("text") : "",
+        details: this.get("details") ? this.get("details") : ""
+      }
+      this.sendAction('update', question, params);
+      this.set('author', '');
+      this.set('text', '');
+      this.set('details', '');
+      this.set('showUpdateQuestion', false);
+    },
+    showUpdateForm() {
+      this.set(this.showUpdateQuestion, false);
+    }
   }
 });
