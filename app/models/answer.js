7bd7ce3147c16258f36a0508d03ce5fcedbdcaa4
Noah Kittleson <noahkittleson@gmail.com>
2016-06-10 11:01:53 -0700

    added answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   text: DS.attr(),
-  details: DS.attr(),
-  answers: DS.hasMany('answer', { async: true})
+  question: DS.belongsTo('question', { async:true })
 });
