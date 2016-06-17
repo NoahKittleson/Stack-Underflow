980d7329256c784c70e8a8864c37976868440529
Noah Kittleson <noahkittleson@gmail.com>
2016-06-17 13:38:23 -0700

    did a little minor styling on answers

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -5,18 +5,10 @@ export default Ember.Component.extend({
   sortBy: ['total:desc'],
 
   actions: {
-    upvote(answer) {
-      var params = {
-        upvotes: answer.get('upvotes') + 1,
-      };
-      console.log("upvotes: " + params.upvotes + " in components/answer-list.js");
+    upvote(params, answer) {
       this.sendAction('upvote', params, answer);
     },
     downvote(answer) {
-      var params = {
-        downvotes: answer.get('downvotes') + 1,
-      };
-      console.log("downvotes: " + params.downvotes + " in components/answer-list.js");
       this.sendAction('downvote', params, answer);
     }
   }
--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    upvote(answer) {
+      var params = {
+        upvotes: answer.get('upvotes') + 1,
+      };
+      console.log("upvotes: " + params.upvotes + " in components/answer-tile.js");
+      this.sendAction('upvote', params, answer);
+    },
+    downvote(answer) {
+      var params = {
+        downvotes: answer.get('downvotes') + 1,
+      };
+      console.log("downvotes: " + params.downvotes + " in components/answer-tile.js");
+      this.sendAction('downvote', params, answer);
+    }
+  }
 });
