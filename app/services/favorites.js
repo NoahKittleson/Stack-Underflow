b33f9da48af1bef23f855173c84d3a8fb52b61cd
Noah Kittleson <noahkittleson@gmail.com>
2016-06-17 16:04:49 -0700

    finished service

--- app/services/favorites.js
+++ app/services/favorites.js
@@ -3,8 +3,13 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   favorites: [],
 
+  getFavs() {
+    return this.get('favorites');
+  },
+
   add(question) {
     this.get('favorites').pushObject(question);
     console.log("added " +  question.id + " to favorites");
+    console.log("there are now " + this.get('favorites').length + " items in favorites");
   }
 });
