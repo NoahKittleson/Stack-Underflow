bae937ec06cb60fd39034cf6fa86598a74564c96
Noah Kittleson <noahkittleson@gmail.com>
2016-06-17 11:05:22 -0700

    fixed the totalVote helper

--- app/helpers/total-vote.js
+++ app/helpers/total-vote.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
-export function totalVote(up, down) {
-  console.log(down);
-  return up - down;
-  // console.log(answer.get('upvotes'));
-  // return answer.get('upvotes') - answer.get('downvotes');
+export function totalVote(params) {
+  var answer = params[0];
+  return answer.get('upvotes') - answer.get('downvotes');
 }
 
 export default Ember.Helper.helper(totalVote);
