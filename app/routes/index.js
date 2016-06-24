f7fd1446602f7dc9bc2d1021eb868ebc214e865e
Noah Kittleson <noahkittleson@gmail.com>
2016-06-23 22:54:17 -0700

    fixed index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   model(){
-    console.log("Sorry about all my screwups.  I committed the wrong branch and then screwed up my code by testing around with it.  --Noah");
     return Ember.RSVP.hash({
-      questions: this.store.findAll('question')
+      questions: this.store.findAll('question'),
     });
   },
-
   actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
